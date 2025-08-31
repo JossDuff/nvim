92e30bb972abb9cde70cae842da2e209ba4cd666
jossduff <jduff360@gmail.com>
2025-08-31 11:59:59 -0400

    cpp lsp

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,58 +1,60 @@
 return {
-  -- Telescope fuzzy finder
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      -- add a keymap to browse plugin files
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
+    -- Telescope fuzzy finder
+    {
+        "nvim-telescope/telescope.nvim",
+        keys = {
+            -- add a keymap to browse plugin files
+            {
+                "<leader>fp",
+                function()
+                    require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+                end,
+                desc = "Find Plugin File",
+            },
+        },
+        opts = {
+            defaults = {
+                layout_strategy = "horizontal",
+                layout_config = { prompt_position = "top" },
+                sorting_strategy = "ascending",
+                winblend = 0,
+            },
+        },
     },
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-      },
+
+    -- Treesitter for syntax highlighting
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "bash",
+                "html",
+                "javascript",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "query",
+                "regex",
+                "tsx",
+                "typescript",
+                "vim",
+                "yaml",
+            },
+        },
     },
-  },
 
-  -- Treesitter for syntax highlighting
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-      },
-    },
-  },
+    -- Comment plugin
+    { "numToStr/Comment.nvim" },
 
-  -- Comment plugin
-  { "numToStr/Comment.nvim" },
-
-  -- VimTeX for LaTeX
-  {
-    "lervag/vimtex",
-    lazy = false,
-    init = function()
-      vim.g.vimtex_view_method = "zathura"
-    end,
-  },
-
+    -- VimTeX for LaTeX
+    -- {
+    --   "lervag/vimtex",
+    --   lazy = false,
+    --   init = function()
+    --     vim.g.vimtex_view_method = "zathura"
+    --   end,
+    -- },
 }
+
