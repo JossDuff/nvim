59386a6a464d25e4a7d8ea76eacb3cc871009f4d
jossduff <jduff360@gmail.com>
2025-09-26 14:09:14 -0400

    don't use relative line numbers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,4 @@
 vim.opt.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for
 vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
 vim.opt.softtabstop = 4 -- Number of spaces that a <Tab> counts for while editing
+vim.opt.relativenumber = false -- show line number, not relative line number
